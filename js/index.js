6eac55a49b4a5aea00b38d987d42d355e39911f9
fisvif <shutkovadim@mail.ru>
2022-01-27 02:42:00 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let body = document.querySelector("body"),
+    headerMenu = document.querySelector(".header-menu-content"),
+    burgerOpenBtm = document.querySelector(".burger"),
+    burgerCloseBtm = document.querySelector(".burger-close");
+
+
+    burgerOpenBtm.onclick = opentBurger;
+    burgerCloseBtm.onclick = closeBurger;
+
+    function opentBurger(){
+        headerMenu.style.display = "flex";
+        body.style.overflow = "hidden";
+    }
+    function closeBurger(){
+        headerMenu.style.display = "none";
+        body.style.overflow = "auto ";
+    }
